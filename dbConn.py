91373cf6096124c5738955c30759224106206771
Cristiana959 <cristiana699@gmail.com>
2021-04-29 12:56:15 +0300

    Added gitignore made a folder

--- dbConn.py
+++
@@ -1,6 +0,0 @@
-from rethinkdb import RethinkDB
-r = RethinkDB()
-r.connect( "localhost", 28015).repl()
-cursor = r.db('mbw').table("id_microbit").run()
-for document in cursor:
-    print(document)
--- dbCreation.py
+++
@@ -1,5 +0,0 @@
-from rethinkdb import RethinkDB
-r = RethinkDB()
-r.connect( "localhost", 28015).repl()
-r.db_create("mbw")
-#r.db('mbw').table_create('id_microbit').run()
