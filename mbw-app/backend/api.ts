e684bf938d1c82305aff7e14df08ec276ed75fe0
Cristiana959 <cristiana699@gmail.com>
2021-05-06 14:55:33 +0300

    Fixed async issue

--- mbw-app/backend/api.ts
+++ mbw-app/backend/api.ts
@@ -2,25 +2,28 @@ import { MongoClient } from "mongodb";
 import express from 'express'
 
 const appRouter = express.Router();
-const uri ="mongodb+srv://cristiana25:1qazse4rfvgy7@mbw.jttju.mongodb.net/mbw?retryWrites=true&w=majority";
 
-const client = new MongoClient(uri, {
-  useUnifiedTopology: true,
-});
 
-appRouter.get('/', async  (req, res) => {
-    const devices = await getDevices();
-    res.send(devices);
-  })
+appRouter.get('/', async (req, res) => {
+  const devices = await getDevices();
+  res.send(devices);
+})
 
 async function getDevices() {
-    
+  const uri = "mongodb+srv://cristiana25:1qazse4rfvgy7@mbw.jttju.mongodb.net/mbw?retryWrites=true&w=majority";
+
+  const client = new MongoClient(uri, {
+    useUnifiedTopology: true,
+  });
+  try {
     await client.connect();
     const database = client.db('mbw');
     const pi = database.collection('pi');
-    const devices =  pi.find({}).toArray();
-  
+    const devices = pi.find({}).toArray();
+
     return devices;
-    
-  }
-  module.exports = appRouter;
+
+  } catch (e) { console.log("Error") } 
+}
+
+module.exports = appRouter;
