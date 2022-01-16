33fedb776a44d3e53822de55ef1e1111368034b5
ib <irina.bradu@stud.fils.upb.ro>
2022-01-16 10:10:28 +0200

    added rot13 decryption

--- mbw-ble/bleSearch.py
+++ mbw-ble/bleSearch.py
@@ -1,6 +1,6 @@
 import pymongo
 import asyncio
-from bleak import discover 
+from bleak import discover
 from datetime import date
 
 
@@ -10,18 +10,33 @@ mydb = myclient.mbw
 pi = mydb.pi
 print(mydb.list_collection_names())
 
+
+def decrypt_rot13(message:str):
+    decrypted = ""
+    for i in range(len(message)):
+        decrypted += chr(ord(message[i]) - 13)
+
+    return decrypted
+
+
+
+
+
 temp = 0
 async def run():
     while True:
         devices = await discover()
         for d in devices:
+
             print(d)
+
+            d.name = decrypt_rot13(d.name)
             #if d.name.startsWith("MB") and d.name[2:].isnumeric()  #uncomment for secure version
             if d.name.startswith("MB"):
                today = date.today()
                d1 = today.strftime("%d/%m/%Y")
                temp = eval(d.name[2:])
-               mydb.pi.find_one_and_update({"id": str(d.address)}, 
+               mydb.pi.find_one_and_update({"id": str(d.address)},
                {"$set":
                    {"id":str(d.address),
                    "name":d.name,
